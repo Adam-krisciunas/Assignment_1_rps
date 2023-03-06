1a1071133e58a3905c962f33cef8a714987943b3
Adam K <112953507+Adam-krisciunas@users.noreply.github.com>
2023-03-06 17:50:54 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,64 @@
 import java.util.Scanner;
 
-public class Main {
+public class RockPaperScissors {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        char playAgain = 'Y';
 
-        while (playAgain == 'Y' || playAgain == 'y') {
-            char playerAMove = getMove(scan, "Player A");
-            char playerBMove = getMove(scan, "Player B");
-            displayResult(playerAMove, playerBMove);
+        Scanner input = new Scanner(System.in);
+        String moveA, moveB, playAgain;
+        boolean playGame = true;
 
-            System.out.print("Would you like to play again [Y/N]? ");
-            playAgain = scan.next().charAt(0);
-
-
-            char move = ' ';
-            while (!(move == 'R' || move == 'P' || move == 'S' || move == 'r' || move == 'p' || move == 's')) {
-                System.out.print(player + " make your move [R, P, S]: ");
-                move = scan.next().charAt(0);
+        while (playGame) {
+            // Get move choice from player A
+            System.out.print("Player A, enter your move (R/P/S): ");
+            moveA = input.nextLine().toUpperCase();
+            while (!moveA.equals("R") && !moveA.equals("P") && !moveA.equals("S")) {
+                System.out.println("Invalid move. Please enter R/P/S: ");
+                moveA = input.nextLine().toUpperCase();
             }
-            return Character.toUpperCase(move);
 
+            // Get move choice from player B
+            System.out.print("Player B, enter your move (R/P/S): ");
+            moveB = input.nextLine().toUpperCase();
+            while (!moveB.equals("R") && !moveB.equals("P") && !moveB.equals("S")) {
+                System.out.println("Invalid move. Please enter R/P/S: ");
+                moveB = input.nextLine().toUpperCase();
+            }
 
-            private static void displayResult ( char playerAMove, char playerBMove){
-                if (playerAMove == playerBMove) {
-                    System.out.println(playerAMove + " vs " + playerBMove + " - it's a tie!");
-                } else if (playerAMove.equalsIgnoreCase("R")) {
-                    if (playerBMove.equalsIgnoreCase('S')) {
-                        System.out.println("Rock breaks Scissors - Player A wins!");
-                    } else {
-                        System.out.println("Paper covers Rock - Player B wins!");
-                    }
-                } else if (playerAMove == 'P') {
-                    if (playerBMove == 'R') {
-                        System.out.println("Paper covers Rock - Player A wins!");
-                    } else {
-                        System.out.println("Scissors cuts Paper - Player B wins!");
-                    }
-                } else if (playerAMove == 'S') {
-                    if (playerBMove == 'P') {
-                        System.out.println("Scissors cuts Paper - Player A wins!");
-                    } else {
-                        System.out.println("Rock breaks Scissors - Player B wins!");
-                    }
+            // Determine the winner
+            if (moveA.equals(moveB)) {
+                if (moveA.equals("S")) {
+                    System.out.println("Scissors vs Scissors it's a Tie!");
+                } else if (moveA.equals("P")) {
+                    System.out.println("Paper vs Paper it's a Tie!");
+                } else {
+                    System.out.println("Rock vs Rock it’s a Tie!");
                 }
+            } else if (moveA.equals("R") && moveB.equals("S")) {
+                System.out.println("Player A wins! Rock smashes scissors.");
+            } else if (moveA.equals("P") && moveB.equals("R")) {
+                System.out.println("Player A wins! Paper covers rock.");
+            } else if (moveA.equals("S") && moveB.equals("P")) {
+                System.out.println("Player A wins! Scissors cuts paper.");
+            } else if (moveB.equals("R") && moveA.equals("S")) {
+                System.out.println("Player B wins! Rock smashes scissors.");
+            } else if (moveB.equals("P") && moveA.equals("R")) {
+                System.out.println("Player B wins! Paper covers rock.");
+            } else if (moveB.equals("S") && moveA.equals("P")) {
+                System.out.println("Player B wins! Scissors cuts paper.");
+            }
+
+            // Ask to play again
+            System.out.print("Play again? (Y/N): ");
+            playAgain = input.nextLine().toUpperCase();
+            while (!playAgain.equals("Y") && !playAgain.equals("N")) {
+                System.out.println("Invalid input. Please enter Y or N: ");
+                playAgain = input.nextLine().toUpperCase();
+            }
+            if (playAgain.equals("N")) {
+                playGame = false;
             }
         }
+        input.close();
     }
 }
